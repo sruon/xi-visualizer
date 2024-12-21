86e57fcb25d57aabaa7db8e9cc97f1bf10a66cf6
InoUno <inoiown@gmail.com>
2024-12-21 01:58:07 +0100

    Remove temporary elements on home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,15 +1,6 @@
 import { A } from "@solidjs/router";
-import { createSignal } from "solid-js";
-import { createStore } from "solid-js/store";
-import AreaMenu, { Area } from "../components/area_menu";
 
 export default function HomePage() {
-  const [getCurrentArea, setCurrentArea] = createSignal<number | undefined>(0);
-  const [areas, setAreas] = createStore<Area[]>([{
-    y: -10,
-    polygon: [],
-  }]);
-
   return (
     <section class="p-8">
       <h1 class="text-2xl font-bold">Home</h1>
@@ -23,18 +14,6 @@ export default function HomePage() {
             <A href="/zone">Zone viewer</A>
           </li>
         </ul>
-      </div>
-      <div
-        style={{ height: "300px", width: "800px", position: "relative", border: "1px solid red" }}
-        onClick={e => {
-          if (e.shiftKey) {
-            const area = areas[getCurrentArea()];
-            setAreas(getCurrentArea(), "polygon", area.polygon.length, { x: e.clientX, z: e.clientY });
-          }
-        }}
-      >
-        <AreaMenu areas={areas} setAreas={setAreas} currentAreaIdx={getCurrentArea()} setCurrentArea={setCurrentArea}>
-        </AreaMenu>
       </div>
     </section>
   );
